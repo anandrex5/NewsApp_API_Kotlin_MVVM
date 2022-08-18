6c6d347ceb527b221898d3e0352892f2abbc8c51
Anand Nishad <90544787+anandrex5@users.noreply.github.com>
2022-08-18 10:49:40 +0530

    Update TestViewModel.kt

--- app/src/main/java/com/example/newsapp1/view/viewmodel/TestViewModel.kt
+++ app/src/main/java/com/example/newsapp1/view/viewmodel/TestViewModel.kt
@@ -32,3 +32,44 @@ class TestViewModel(val repository: PostRepository): ViewModel() {
     }
 
 }
+
+//if you want to add status code then replace below code  from fun getAllData() method -
+
+ fun getAllData(){
+        val response = repository.getPost()
+        response.enqueue(object : Callback<List<PojoItemItem>> {
+            override fun onResponse(
+                call: Call<List<PojoItemItem>>,
+                response: Response<List<PojoItemItem>>
+            ) {
+
+                val statusCode = response.code()
+                when (statusCode) {
+                    200 -> {
+
+//                        postList.postValue(response.body())
+//                         postList.value = (response.body())
+//                     }
+//                     400 -> {
+//                         Log.e("code","Error")
+
+//                     }
+//                     500 -> {
+//                         Log.e("code","Internal Server request ")
+//                     }
+//                     401 -> {
+//                         Log.e("code","Authentication Fail")
+//                     }
+
+//                     else -> {
+
+//                     }
+//                 }
+//             }
+//             override fun onFailure(call: Call<List<PojoItemItem>>, t: Throwable) {
+//                 Log.e("Fail", "${t.message}")
+//             }
+//         })
+//     }
+
+// }
